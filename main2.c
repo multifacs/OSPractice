d60d74ca6f706317110551bd92462fc91888ad95
MultiFaCs <multifacs@users.noreply.github.com>
2021-04-12 23:23:04 +0300

    Update main2.c

--- main2.c
+++ main2.c
@@ -14,40 +14,67 @@ int main(void) {
 
     prev_pipe = STDIN_FILENO;
 
-    pipe(pfds);
 
+    pipe(pfds);
     if (fork() == 0) {
+        // Redirect previous pipe to stdin
+        if (prev_pipe != STDIN_FILENO) {
+            printf("NO1\n");
+            dup2(prev_pipe, STDIN_FILENO);
+            close(prev_pipe);
+        }
+        // Redirect stdout to current pipe
         dup2(pfds[1], STDOUT_FILENO);
         close(pfds[1]);
-
+        // Start command
         execvp(commands[0][0], commands[0]);
+        perror("execvp failed");
+        exit(1);
     }
-
+    // Close read end of previous pipe (not needed in the parent)
     close(prev_pipe);
+    // Close write end of current pipe (not needed in the parent)
     close(pfds[1]);
-
+    // Save read end of current pipe to use in next iteration
     prev_pipe = pfds[0];
 
+
+
     pipe(pfds);
-
     if (fork() == 0) {
+        // Redirect previous pipe to stdin
+        if (prev_pipe != STDIN_FILENO) {
+            printf("NO2\n");
+            dup2(prev_pipe, STDIN_FILENO);
+            close(prev_pipe);
+        }
+        // Redirect stdout to current pipe
+        dup2(pfds[1], STDOUT_FILENO);
+        close(pfds[1]);
+        // Start command
+        execvp(commands[1][0], commands[1]);
+        perror("execvp failed");
+        exit(1);
+    }
+    // Close read end of previous pipe (not needed in the parent)
+    close(prev_pipe);
+    // Close write end of current pipe (not needed in the parent)
+    close(pfds[1]);
+    // Save read end of current pipe to use in next iteration
+    prev_pipe = pfds[0];
 
+
+
+    // Get stdin from last pipe
+    if (prev_pipe != STDIN_FILENO) {
+        printf("NO3\n");
         dup2(prev_pipe, STDIN_FILENO);
         close(prev_pipe);
-
-        dup2(pfds[1], STDOUT_FILENO);
-        close(pfds[1]);
-
-        execvp(commands[1][0], commands[1]);
     }
 
-    close(prev_pipe);
-    close(pfds[1]);
-
-    prev_pipe = pfds[0];
-
-    dup2(prev_pipe, STDIN_FILENO);
-    close(prev_pipe);
-
+    // Start last command
     execvp(commands[2][0], commands[2]);
+
+    perror("execvp failed");
+    exit(1);
 }
